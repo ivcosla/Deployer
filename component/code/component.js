cf947b7e65dfaf33f2827bd50bf6c1983bc40580
Iván Costa <ivcoslaj@gmail.com>
2015-10-23 20:29:14 +0200

    more

--- component/code/component.js
+++ component/code/component.js
@@ -1,4 +1,8 @@
-outSocket.send("Hola");
-inSocket.on("message",function(mes){
+var fs=require(fs);
+var pack_json=fs.readSync('../package.json','utf8');
+var component = JSON.parse(pack_json);
+
+component.outSocket.send("Hola");
+component.inSocket.on("message",function(mes){
 	console.log(mes);
 })
