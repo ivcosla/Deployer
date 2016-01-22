d5f99c0881f5caba10a91af761870f1047bd596f
Iván Costa <ivcoslaj@gmail.com>
2016-01-22 15:34:30 +0100

    Started graph parser for creation of channels

--- deployment/code/utilities/graphParser.js
+++ deployment/code/utilities/graphParser.js
@@ -1,8 +1,30 @@
 // TODO: Parse graph and create channels
-var parse = function(sJson){
+
+var projectPath = '../../'
+
+// Asign a port to each component endpoint
+// port range: 50000 + 
+function setCompPorts(sJson,paths){
+    var initPort = 50000;
+    // resList: Dictionary of dictionaries -> {comp:{ (endpoint:port)* }}
+    var resList;
+    var portIndex = 0;
+    for (var comp in sJson.components){
+        var cJson = require(paths[comp]+'/package.json');
+        
+        var inConns = cJson.connections['input-connection'];
+        resList[comp] = {}         
+        for (var endp in inConns){
+            resList[comp][endp] = initPort + portIndex; portIndex++;
+        }
+                       
+    }
+}
+
+function parse(sJson){
     var graph = sJson.graph;
     
-    for(var i = 0; i < graph.length; i++){
-        
+    for (var channel in graph){
+        //channel.source.        
     }
 }
